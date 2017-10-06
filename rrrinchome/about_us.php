bbbcfdb49d2fea13b4b0cb581d3735c2f2f24d53
ggdetkos <ggdetkos@gmail.com>
2017-10-06 16:38:55 -0400

    Test commit RRRInc home and about

---
+++ rrrinchome/about_us.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<?php
+
+print "
+<html>
+
+	<head>
+";
+
+require ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/headTagsAllPages.php");
+
+print"
+		<meta name='dcterms.created' content='Mon, 04 Aug 2014 16:45:00 GMT'>
+";
+
+require ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/HeadLinksAllPages.php");
+
+print "
+		<title>About Us | Rhodesian Ridgeback Rescue, Inc.</title>
+
+	</head>
+
+	<body>
+
+<div id='page'>
+
+<div id='navColumn'>";
+
+require ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/navColumnIncludes.php");
+
+print "
+</div>
+
+<div id='pageContentAndMugs'>
+
+<div id='pageContent' class='print'>
+
+<div id='pageTitle'>
+About Us
+</div>
+
+<div id='pageSubTitle'>
+About Rhodesian Ridgeback Rescue, Inc.
+</div>
+
+<div class='sectionContent'>
+<div class='sectionTitle'>Our Mission</div>
+Rhodesian Ridgeback Rescue, Inc., (RRRInc) is a not-for-profit, charitable organization dedicated to the rescue, care, and adoption of abandoned or unwanted Rhodesian Ridgeback dogs, as well as to protecting and preserving the integrity and welfare of the Rhodesian Ridgeback breed.
+</div>
+
+<div class='sectionContent'>
+<div>Proud to be...</div>
+<div class='allVolunteerOrg'>An All-Volunteer Organization!</div>
+</div>
+
+<div class='sectionContent'>
+RRRInc is an all-volunteer organization that includes breeders, owners, and other devotees of the breed. RRRInc was established under Section <span class='no_break'>501(c)(3)</span> of the United States Internal Revenue Code.
+</div>
+
+<div class='sectionContent'>
+Rhodesian Ridgeback Rescue, Inc., is the official national rescue program of the <a href='http://www.akc.org/' target='_blank'>AKC</a>-recognized US national breed club, the <a href='http://www.rrcus.org/' target='_blank'>Rhodesian Ridgeback Club of the United States, Inc., (RRCUS)</a>, and the <a href='http://www.ckc.ca/' target='_blank'>CKC</a>-recognized Canadian national breed club, the <a href='http://www.rrclubofcanada.org/' target='_blank'>Rhodesian Ridgeback Club of Canada (RRCC)</a>. RRRInc is also the official rescue program for most local and regional Rhodesian Ridgeback clubs across the USA and Canada.
+</div>
+
+<div class='sectionContent'>
+Rhodesian Ridgeback Rescue, Inc., is comprised of a network of <a href='coordinators.html' target='_top'>volunteers</a> across the USA and Canada, and operates under an established set of policies, procedures, and guidelines. RRRInc volunteers endeavour to assist Ridgeback owners in raising happy healthy dogs by providing information on the breed, and assisting with training and care throughout a dog's life.
+</div>
+
+<div class='sectionContent'>
+<div id='history' class='sectionTitle'>Our History</div>
+In 2005, Rhodesian Ridgeback Rescue, Inc., celebrated 35 years of helping Rhodesian Ridgebacks in need! From its beginnings in 1970 as the adoption committee of the Rhodesian Ridgeback Club of the United States, Inc., (RRCUS) until today, RRRInc has evolved into a responsible and efficient, while at the same time compassionate, organization capable of rescuing and providing care for hundreds of Rhodesian Ridgebacks every year across the USA and Canada. RRRInc was spun off from RRCUS in 2000 as a separate corporation with separate officers and directors, but has remained to this day RRCUS's official rescue program. RRRInc obtained its status as an IRS 501(c)(3) charitable organization at the time of incorporation, which allowed for donations to be tax deductible for the first time.
+</div>
+
+<div class='sectionContent'>
+Since 2000, RRRInc has greatly expanded its volunteer base. It has also successfully reached out to the worldwide community of Rhodesian Ridgeback owners who, through their never-ending generosity and love for the breed, provide regular, annual financial support to Rhodesian Ridgeback Rescue, Inc. RRRInchas also since become the official rescue program for the Rhodesian Ridgeback Club of Canada, as well as most local and regional Rhodesian Ridgeback clubs. Rather than each club operating its own rescue program, they now take advantage of the efficiencies, experience, and resources of one national program. This has allowed RRRInc to grow larger and more capable of caring for all rescued dogs, but especially those with special needs, such as expensive surgery and rehabilitation. RRRInc's goal is to provide care for all rescued Rhodesian Ridgebacks in need. RRRInc does not turn away any dogs because it cannot afford them. Since 2000, RRRInc has accepted more than 4,000 Ridgebacks into its rescue program.
+</div>
+
+<div class='sectionContent'>
+<div id='msaada' class='sectionTitle'>The Msaada Ridgebacks</div>
+<span class='msaada'>Msaada</span> is the name used by Rhodesian Ridgeback Rescue, Inc., for its family of rescued dogs. <span class='msaada'>Msaada</span> means &quot;aid, assistance, help, or support&quot; in the Swahili language.
+</div>
+
+<div class='sectionContent'>
+<div id='volunteer' class='sectionTitle'>Volunteer with Us!</div>
+If you are interested in volunteering with RRRInc, there are many different opportunities available. They include certain State, Provincial, and Area Rescue Coordinator positions; fostering and transporting dogs; talking with adoption applicants, animal shelters and others about the breed; fundraising; and administrative support tasks, such as bookkeeping and record-keeping. If you are interested in any of these or other volunteer opportunities, then please visit our <a href='how_you_can_help.html' target='_top'>How You Can Help!</a> page and submit an online Volunteer Application today!
+</div>
+
+<div class='sectionContent'>
+<div id='officers' class='sectionTitle'>Officers and Directors</div>
+<div class='sectionSubTitle'>President</div>
+<div>Cheryl Fraser</div>
+<div>tel: 508.393.6966</div>
+<div><a href='forms/contact_us.php?RRRIncName=cfraser&amp;regioncode=admexec' target='_blank'>Email Cheryl...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Vice President</div>
+<div>Nicolina Devito</div>
+<div>tel: 770.594.8819</div>
+<div><a href='forms/contact_us.php?RRRIncName=ndevito&amp;regioncode=admbrd' target='_blank'>Email Niki...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Treasurer</div>
+<div>Elizabeth Goodman</div>
+<div>tel: 571.236.2845</div>
+<div><a href='forms/contact_us.php?RRRIncName=egoodman&amp;regioncode=admexec' target='_blank'>Email Elizabeth...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Secretary</div>
+<div>Lorraine Pedersen</div>
+<div>tel: 425.377.9453</div>
+<div><a href='forms/contact_us.php?RRRIncName=lpedersen&amp;regioncode=admexec' target='_blank'>Email Lorraine...</a></div>
+</div>
+
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Directors</div>
+<div>Aubrey Beretta</div>
+<div>tel: 503.777.4945</div>
+<div><a href='forms/contact_us.php?RRRIncName=aberetta&amp;regioncode=admbrd' target='_blank'>Email Aubrey...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Alyssa Moon-Dyke</div>
+<div><a href='forms/contact_us.php?RRRIncName=amoondyke&amp;regioncode=admbrd' target='_blank'>Email Alyssa...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Jill Pickering</div>
+<div><a href='forms/contact_us.php?RRRIncName=jpickering&amp;regioncode=admbrd' target='_blank'>Email Jill...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Terry Sheinfield</div>
+<div>tel: 314.805.6781</div>
+<div><a href='forms/contact_us.php?RRRIncName=tshienfield&amp;regioncode=admbrd' target='_blank'>Email Terry...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Timothy Smaha</div>
+<div><a href='forms/contact_us.php?RRRIncName=tsmaha&amp;regioncode=admbrd' target='_blank'>Email Tim...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Marlene Strandt</div>
+<div>tel: 469.931.2934</div>
+<div><a href='forms/contact_us.php?RRRIncName=mstrandt&amp;regioncode=admbrd' target='_blank'>Email Marlene...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Dennis Wolf</div>
+<div>tel: 718.381.0500</div>
+<div><a href='forms/contact_us.php?RRRIncName=dwolf&amp;regioncode=admbrd' target='_blank'>Email Dennis...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>RRCUS Directorship</div>
+<div>Christine Kitsch</div>
+<div><a href='forms/contact_us.php?RRRIncName=ckitsch&amp;regioncode=admbrd' target='_blank'>Email Christine...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Non-Voting Members</div>
+<div>Carole Bradley-Kennedy (Past President)</div>
+<div>tel: 905.659.6971</div>
+<div><a href='forms/contact_us.php?RRRIncName=ckennedy&amp;regioncode=admbrd' target='_blank'>Email Carole...</a></div>
+</div>
+
+<div class='sectionContent'>
+<div>Nicole Tate (Past President)</div>
+<div>tel: 803.754.9732</div>
+<div><a href='forms/contact_us.php?RRRIncName=ntate&amp;regioncode=admbrd' target='_blank'>Email Nicole...</a></div>
+</div>
+
+<!-- NOT DISPLAYED -->
+
+<div style='display:none;' class='sectionContent'>
+<div id='reports_and_documents' class='sectionTitle'>Reports and Documents</div>
+
+<div style='display:none;' class='sectionSubTitle'>U.S. Internal Revenue Service Form 990</div>
+<a href='http://get.adobe.com/reader/' target='_blank'><img src='images/pdficon_small.png' alt='PDF Icon' /></a> 
+<a href='special/RhodesianRidgebackRescueInc2013Form990.pdf' target='_blank'>2013</a> ||
+<a href='special/RhodesianRidgebackRescueInc2012Form990.pdf' target='_blank'>2012</a> ||
+<a href='special/RhodesianRidgebackRescueInc2011Form990.pdf' target='_blank'>2011</a>
+</div>
+
+<div style='display:none;' class='sectionContent'>
+<div class='sectionSubTitle'>Corporate Documents</div>
+<a href='http://get.adobe.com/reader/' target='_blank'><img src='images/pdficon_small.png' alt='PDF Icon' /></a> 
+<a href='special/articles_of_incorporation.pdf' target='_blank'>Articles of Incorporation</a> ||
+<a href='special/bylaws.pdf' target='_blank'>Bylaws</a> ||
+<a href='special/irs_form_1023_501(c)(3)_application.pdf' target='_blank'>IRS Form 1023</a>
+
+</div>
+
+<!-- End NOT DISPLAYED -->
+
+<div class='sectionContent'>
+<div class='sectionSubTitle'>Request a Copy of Our Latest Financial Report</div>
+<div>Rhodesian Ridgeback Rescue, Inc.</div>
+<div>PO Box 4141</div>
+<div>Leesburg VA 20177-8287</div>
+<div>USA</div>
+</div>
+
+<div class='sectionContent'>
+<div>Elizabeth Goodman, Treasurer</div>
+<div><a href='forms/contact_us.php?RRRIncName=egoodman&amp;regioncode=admexec' target='_blank'>Email Elizabeth...</a></div>
+</div>
+
+<div class='sectionContent'>
+<a href='notices/copyright.html' target='_top'>Copyright Notice</a>
+</div>
+
+</div>
+
+<div id='randomMugColumn'>";
+
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+include ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/dogMugRandomizer.php");
+
+print "
+</div>
+
+</div>
+";
+
+require ("/var/www/vhosts/ridgebackrescue.org/httpdocs/php/footer.php");
+
+print "
+
+</div>
+
+	</body>
+
+</html>";
+
+?>
